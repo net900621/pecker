d358770e1e8459e110e2bba0160952f6bfa26d53
yaoyao <net900621@163.com>
2015-07-07 16:46:56 +0800

    easy demo

--- Test.php
+++ Test.php
@@ -3,28 +3,21 @@ class ShouQ_banner_square_Test extends PHPUnit_Framework_TestCase
 {
     public function testTypeOfResponse()
     {
+    	require_once(dirname(__FILE__) . '/conf/envConf');
+        require_once(dirname(__FILE__) . '/caseConf/ShouQ_banner_square_conf.php');
+		$env = new env();
+		$host = $env->hosts;
+		$_conf = new base_conf();
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "http://snake.yaoyao.rdlab.meilishuo.com/shouq/ShouQ_banner_square");
+		curl_setopt($ch, CURLOPT_URL, $host . "/shop/shop_follow?shop_id=1003802");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_COOKIE, $_conf->cookie);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		$output = curl_exec($ch);
 		curl_close($ch);
 		$output = json_decode($output);
-		$type = gettype($output);
-		$this->assertEquals("array", $type);
+		$res = $output->code;
+		$this->assertEquals(0, $res);
     }
 	
-
-    public function noneTypeOfResponse()
-    {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "http://snake.yaoyao.rdlab.meilishuo.com/shouq/ShouQ_banner_square");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		$output = curl_exec($ch);
-		curl_close($ch);
-		$output = json_decode($output);
-		$res = $output[0]->imgurl;
-		$this->assertEquals("http://d04.res.meilishuo.net/img/_o/df/4e/578ff41ccd89e34dabbd0212c0f5_315_451.cg.jpg", $res);
-    }
 }
